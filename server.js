0e8c30cf72e9303660979db048d4469113831759
Dennis yee <ykkadmin+github@gmail.com>
2014-12-05 12:25:32 -0800

    removed keys

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ var morgan     = require('morgan');
 var api        = require('marvel-api');
 
 var marvel = api.createClient({
-  publicKey: 'ae0730cc58984431c3ef197f6f10eacc',
-  privateKey: 'e758db63116d494729308a3b1aa9dec62cc1b107'
+  publicKey: '',
+  privateKey: ''
 });
 
 // configure app
